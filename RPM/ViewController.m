f84adc161a7615253431ea45d81b50beab3149d6
Oliver Andrews <oltandrews@gmail.com>
2015-05-14 13:47:26 -0700

    fin

--- RPM/ViewController.m
+++ RPM/ViewController.m
@@ -10,13 +10,46 @@
 
 @interface ViewController ()
 
+
+@property (weak, nonatomic) IBOutlet UIImageView *needleImage;
+
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+#define degreesToRadians(degrees) (M_PI * degrees / 180.0)
+    
+    self.needleImage.transform = CGAffineTransformMakeRotation(degreesToRadians(120));
+    
+    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(didPan:)];
+    
+    [self.view addGestureRecognizer:panGesture];
+}
+
+- (void)didPan:(UIPanGestureRecognizer *)sender {
+    CGPoint velocity = [sender velocityInView:self.view];
+    CGFloat vx = velocity.x;
+    CGFloat vy = velocity.y;
+    
+    double velocityVector = sqrt(vx*vx + vy*vy );
+    NSLog(@"%f",velocityVector);
+    double angle = 120.0 + ((velocityVector/3000)*300.0);
+    
+    self.needleImage.transform = CGAffineTransformMakeRotation(degreesToRadians(angle));
+    
+    if (sender.state == UIGestureRecognizerStateBegan) {
+        // not doing anything here right now.
+    } else if (sender.state == UIGestureRecognizerStateEnded) {
+        self.needleImage.transform = CGAffineTransformMakeRotation(degreesToRadians(120));
+    }
+    
+
 }
 
 - (void)didReceiveMemoryWarning {
